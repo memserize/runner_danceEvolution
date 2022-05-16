b133df8a895f4b87356698b89708d9ac4b7833d2
joshuavanbreda <joshuavanbreda@gmail.com>
2022-05-16 22:51:23 +0200

    screenshot working

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public bool gameRunning;
+
+    public UnityEvent OnLevelEnd;
 
 
     Refrences refrences;
